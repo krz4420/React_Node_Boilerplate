d9bc72ce7212f8aec4d9aaa1f8eab687d33f2c62
Kyle Zawadzki <kyle.zawadzki@yale.edu@users.noreply.github.com>
2021-08-03 00:27:51 -0400

    Add user button now only shows up if there are more than 1 user in the call

--- src/AddUser.js
+++ src/AddUser.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class AddUser extends React.Component {
   render() {
-    return this.props.users != 0 ? (
+    return this.props.users > 1 ? (
       <button class="ui basic button" onClick={this.props.handleOnClick}>
         <i class="icon plus"></i>
         Click To Add User "{this.props.users}"
